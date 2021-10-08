d3a4e358a6a13b24149f1871a0d31cead911f4dd
kaiorod <caiorodrigo2775@gmail.com>
2021-10-08 20:12:20 -0300

    Finaliza Sinal

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace cs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n;
+
+            Console.Write("Digite um número : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n < 0)
+            {
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Negativo");
+                Console.ResetColor();
+            }
+            else if (n == 0)
+            {
+                Console.WriteLine("Zero");
+            }
+            else if (n > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Positivo");
+                Console.ResetColor();
+            }
+
+
         }
     }
 }
